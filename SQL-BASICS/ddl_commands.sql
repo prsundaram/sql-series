52671299d8e5ef8d6515dd9f364c5c0bc2e5c568
Prasanna Sundaram <prasannaa.sundaram@hotmail.com>
2025-09-10 22:30:38 +0530

    Adding DDL and DML examples

---
+++ SQL-BASICS/ddl_commands.sql
@@ -0,0 +1,33 @@
+create database mysql_series; -- Create a database
+
+commit; -- commit it
+
+use mysql_series; -- select the database
+
+-- Create table
+
+create table student(id int, student_name varchar(100), age int);
+
+select * from student;
+
+-- Alter table
+
+alter table student add column city varchar(10);
+
+-- Insert values
+insert into students(id, student_name, age, city) values (1, "Prasanna Sundaram", 35, "Chennai");
+
+-- Rename the table 
+rename table student to students;
+
+-- Read the records
+select * from students;
+
+-- Truncate --> Removes all the record from the table, however, it retains the table structure
+truncate table students;
+
+select * from students;
+
+-- Delete the table
+
+drop table students;
